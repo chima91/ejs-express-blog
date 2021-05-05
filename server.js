eccc3f7cd0d4475ae6aa9783a5d66fa38b0c1996
Kazuma NAGAI <itb4769we@gmail.com>
2021-05-05 22:24:28 +0900

    routing & statusCode & redirect

--- server.js
+++ server.js
@@ -2,16 +2,36 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+    }
+
 
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err);
             res.end();
         } else {
-            res.write(data);
-            res.end();
+            // res.write(data);
+            res.end(data);
         }
     })
 });
