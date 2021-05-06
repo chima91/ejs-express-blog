73bfdfa607b884e91b013db9fa815a8c350a6cd6
Kazuma NAGAI <itb4769we@gmail.com>
2021-05-06 18:16:03 +0900

    npm i -g -D --save

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method);
+
+    const num = _.random(0, 20);
+    console.log(num);
+
     let path = './views/';
     switch(req.url) {
         case '/':
@@ -13,7 +18,7 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
