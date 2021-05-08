0d49dc1267997404f256d475f6b2d6fcc9a77e66
Kazuma NAGAI <itb4769we@gmail.com>
2021-05-08 09:54:03 +0900

    prepare for server-side rendering

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'home', blogs });
 });
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'about' });
 });
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 })
