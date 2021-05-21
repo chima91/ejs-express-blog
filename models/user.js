7a4dfae524c8b5b44f2a23ecc8d6ed759f42b242
Kazuma NAGAI <itb4769we@gmail.com>
2021-05-21 16:42:18 +0900

    'mongoose hooks' を用いてパスワードをハッシュ値に変換してDBに登録する実装

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,12 @@ const userSchema = new mongoose.Schema({
         required: [true, 'パスワードを入力してください。'],
         minlength: [6, 'パスワードは6文字以上にしてください。']
     }
+});
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.passwd = await bcrypt.hash(this.passwd, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
